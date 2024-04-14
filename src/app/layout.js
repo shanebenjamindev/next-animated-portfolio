e4da89405e9cf2f51fd9d272da82183b9da7c8fb
Your Name <you@example.com>
2024-04-14 18:26:01 +0700

    fixing small device layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.scss";
+import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
